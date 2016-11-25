6856643c82997a2538b7aa7d1d812eaab93f034f
Amos47 <jordan.amos@shopify.com>
2016-11-24 22:26:45 -0500

    make views pretty

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  Project.create!(
+    title: "#{Faker::Hacker.verb} the #{Faker::Hacker.adjective} #{Faker::Hacker.noun}".titleize,
+    description: Faker::Hacker.say_something_smart,
+    publish_at: Time.now.utc
+  )
+end

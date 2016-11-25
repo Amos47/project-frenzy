ee22d4022612befe76d5a2be2489c92001c2c4d4
Amos47 <jordan.amos@shopify.com>
2016-11-25 00:32:34 -0500

    this might work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125013526) do
+ActiveRecord::Schema.define(version: 20161125041144) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20161125013526) do
     t.time     "publish_at"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

008e32da65cd58a1cb5cc74032c9cd49d4be2dc4
Amos47 <jordan.amos@shopify.com>
2016-12-03 09:36:42 -0500

    db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 prof = Professor.create!(
   name: "Dr. #{Faker::Name.name}",
-  email: "p@test.com",
-  password: "password",
-  password_confirmation: "password"
-)
-student = Student.create!(
-  name: "#{Faker::Name.name}",
-  email: "s@test.com",
+  email: "p1@test.com",
   password: "password",
   password_confirmation: "password"
 )
 
-10.times do
+1.upto(10) do |i|
+  Student.create!(
+  name: "#{Faker::Name.name}",
+  email: "s#{i}@test.com",
+  password: "password",
+  password_confirmation: "password"
+  )
+
   Project.create!(
-    title: "#{Faker::Hacker.verb} the #{Faker::Hacker.adjective} #{Faker::Hacker.noun}".titleize,
-    professor: prof,
-    description: Faker::Hacker.say_something_smart,
-    publish_at: Time.now.utc
+  title: "#{Faker::Hacker.verb} the #{Faker::Hacker.adjective} #{Faker::Hacker.noun}".titleize,
+  professor: prof,
+  description: Faker::Hacker.say_something_smart,
+  publish_at: Time.now.utc
   )
 end
